122bde319b2aae09a706fee7f283f7efa799d3b5
Milan Contreras <milan.contreras@epn.edu.ec>
2022-08-07 15:00:40 -0500

    part2.10

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,127 @@
+import React, { useState } from 'react'
+
+const Persons = ({persons})=>{
+  return (
+    persons.map((person) => (<Person key ={person.name} person={person.name} number={person.number}></Person>)))
+}
+
+const Person = ({person, number})=>{
+  return (<p>{person} {number}</p>)
+}
+
+const Filter =({value, onChange}) => {
+  return (
+    <p>
+      filter shown with: <input value= {value} onChange={onChange}/>
+    </p>
+    )
+}
+
+const PersonForm =({onSubmit,valueName,valueNumber,onChangeName,onChangeNumber})=>{
+  return (
+    <div>
+      <form onSubmit={onSubmit}>       
+        <div>
+          name: <input value = {valueName} onChange={onChangeName}/>
+        </div>
+        <div> 
+        number: <input value= {valueNumber} onChange={onChangeNumber}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </div>
+
+  )
+}
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [filteredPersons , setFilteredPersons] =useState(persons)  
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setFilter ] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  
+  const handleFilterChange = (event) => {
+    const inputValue = event.target.value
+    setFilter(inputValue)
+    //console.log("nuevo: "+inputValue)
+    const re = new RegExp(`^${inputValue.toUpperCase()}.*`);
+    const list = persons.filter(person =>{
+      if(person.name.toUpperCase().match(re)){
+        console.log("nombre: "+person.name)
+      }
+      return (
+        person.name.toUpperCase().match(re)
+      )
+    })
+    // console.log('newFilter :>> ', inputValue);
+    // console.log('re :>> ', re);
+    // console.log('list :>> ', JSON.stringify(list));
+    setFilteredPersons(list)
+
+  }
+
+  const addName = (event) => {
+    event.preventDefault();
+    const nameObject ={
+      name: newName,
+      number: newNumber
+    }
+    
+    const repetidos = persons.find(persons=>{
+      return persons.name === nameObject.name
+    })
+
+    if(repetidos !== undefined){
+      alert(`${newName} is already added to phonebook`)
+    }else{
+      const arrayNames = [...persons, nameObject]
+      setPersons(arrayNames)
+      setNewName("")
+      setNewNumber("")
+    }
+   
+  }
+
+
+  const personsToDisplay = (newFilter === '' ? persons : filteredPersons)
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter value= {newFilter} onChange={handleFilterChange}/>
+
+      <h3>Add a new</h3>
+      <PersonForm 
+        onSubmit={addName} 
+        valueName = {newName} 
+        valueNumber= {newNumber} 
+        onChangeName={handleNameChange} 
+        onChangeNumber={handleNumberChange}
+      />
+      
+      <h3>Numbers</h3>
+
+      <Persons persons={personsToDisplay}
+      />
+
+    </div>
+  )
+}
+
+export default App
