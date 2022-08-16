9891c0197c63a45e7a2e2c185cf16dc941b3ab0c
Milan Contreras <milan.contreras@epn.edu.ec>
2022-08-16 01:08:08 -0500

    part4.20

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -19,11 +19,13 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connecting to MongoDB:', error.message)
   })
 
+
+
 app.use(cors())
 //app.use(express.static('build'))
 app.use(express.json())
 
-
+app.use(middleware.tokenExtractor)
 app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
